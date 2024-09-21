959ebdf2168c54c98f8242696052a29dad8f91ad
Iamrishabhnirmalkar <iamrishabhnirmalkar@gmail.com>
2024-09-22 02:54:37 +0530

    feat: button commponents

--- src/components/ui/Button.UI/Button.UI.tsx
+++ src/components/ui/Button.UI/Button.UI.tsx
@@ -2,27 +2,30 @@ import React from 'react'
 import { cn } from '@/utils/libs/cn'
 
 interface ButtonUIProps {
-    text: string
+    text?: string
+    icon?: React.ReactNode
     variant: 'primary' | 'secondary' | 'tertiary'
+    isIconButton?: boolean
 }
 
-export default function ButtonUI({ text, variant }: ButtonUIProps) {
+export default function ButtonUI({ text, icon, variant, isIconButton }: ButtonUIProps) {
     function getVariantStyle(variant: 'primary' | 'secondary' | 'tertiary') {
         switch (variant) {
             case 'primary':
-                return 'bg-btn-primary'
+                return 'bg-btn-primary text-btn-primaryText hover:bg-btn-primaryHover border-btn-primaryBorder shadow-btnPrimary'
             case 'secondary':
-                return 'bg-btn-secondary'
+                return 'bg-btn-secondary text-btn-secondaryText hover:bg-btn-secondaryHover border-btn-secondaryBorder shadow-btnSecondary'
             case 'tertiary':
-                return 'bg-btn-tertiary'
+                return 'bg-btn-tertiary text-btn-tertiaryText hover:bg-btn-tertiaryHover border-btn-tertiaryBorder shadow-btnTertiary'
             default:
                 return ''
         }
     }
 
     return (
-        <button className={cn('text-center px-5 py-2 rounded-lg', getVariantStyle(variant))}>
-            <p>{text}</p>
+        <button className={cn('flex items-center justify-center px-5 py-2 rounded-lg', getVariantStyle(variant))}>
+            {isIconButton && icon && <span className="mr-2">{icon}</span>}
+            {text && <p>{text}</p>}
         </button>
     )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,31 +8,32 @@ const config: Config = {
         extend: {
             colors: {
                 backgroundColor: '#ffffff',
-
-                //
                 primary: '#ffffff',
                 secondary: '#000000',
                 tertiary: '#000000',
                 btn: {
                     //================Primary Button Colors ========
                     primary: '#123456',
-                    primaryHover: '',
-                    primaryBorder: '',
-                    primaryShadow: '',
-                    primaryText: '',
+                    primaryHover: '#000000',
+                    primaryBorder: '#000000',
+                    primaryText: '#000000',
                     //================Secondary Button Colors ========
                     secondary: '#123456',
-                    secondaryHover: '',
-                    secondaryBorder: '',
-                    secondaryShadow: '',
-                    secondaryText: '',
+                    secondaryHover: '#000000',
+                    secondaryBorder: '#000000',
+                    secondaryText: '#000000',
                     //================Tertiary Button Colors ========
                     tertiary: '#123456',
-                    tertiaryHover: '',
-                    tertiaryBorder: '',
-                    tertiaryShadow: '',
-                    tertiaryText: ''
+                    tertiaryHover: '#000000',
+                    tertiaryBorder: '#000000',
+                    tertiaryText: '#000000'
                 }
+            },
+            boxShadow: {
+                // ==========btn Shadow
+                btnPrimary: '',
+                btnSecondary: '',
+                btnTertiary: ''
             },
             screens: {
                 xs: '480px', // Extra small devices (portrait phones)
@@ -50,5 +51,6 @@ const config: Config = {
     },
     plugins: []
 }
+
 export default config
 
